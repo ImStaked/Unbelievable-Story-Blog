a438148abef6e9a0a83ef3275bed4cdc7fe8839f
ImStaked <[admin@imstaked.com]>
2026-03-10 11:53:17 -0500

    Updates

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -9,8 +9,11 @@ export function Intro() {
       <center>
         <br />
           I made this because I am constantly under attack by weird electronic weapons and have been since 2019 
-          Also it looks like someone in the government has been using me in their operations without my knowledge or consent. 
+        <br />
+          Also it looks like someone in the government has been using me in their operations without my knowledge or consent.
+        <br />
           It is not possible to be involved with this many high profile things at random.
+        <br />
           Now I think they have been trying to kill me to cover up for what they have been doing to me.
         <br /> 
     <br />
